32dddeb96f3754ae50c2615fd39af9ec3caaec6c
Bruno Candido <bmcandido14@gmail.com>
2020-04-07 20:44:01 -0300

    78. Esqueci a senha

--- src/main/java/com/brunocandido/cursomc/config/SecurityConfig.java
+++ src/main/java/com/brunocandido/cursomc/config/SecurityConfig.java
@@ -47,10 +47,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = {
 
 			"/produtos/**", "/categorias/**" };
-	
+
 	private static final String[] PUBLIC_MATCHERS_POST = {
 
-			 "/clientes/**" };
+			"/clientes/**", "/auth/forgot/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -60,10 +60,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		}
 
 		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-		         .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-		         .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-				 .antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
+				.anyRequest().authenticated();
 		// Autenticando
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		// Autorizando
